01dbb03f3532c3339d23a6100b95a799a64c3666
alber <aicarq@hotmail.com>
2023-05-27 15:45:25 -0600

    background obscured add js file

---
+++ app.js
@@ -0,0 +1,14 @@
+const onlyButton = document.getElementsByTagName("button");
+const onlyButton2 = document.getElementById("the-only-button");
+const passwordInput = document.getElementById("pass");
+const confirmPass = document.getElementById("confirm"); 
+
+onlyButton2.addEventListener("click", consolation());
+
+
+
+function consolation() {
+    console.log("clicked");
+}
+
+console.log("LOGGED ON?")
