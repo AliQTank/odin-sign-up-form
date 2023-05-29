181110b7e00d9b4c54f1b6a1bc565171715b1fa5
Alber <aicarq@hotmail.com>
2023-05-29 05:17:48 -0600

    added style only

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const onlyButton = document.getElementsByTagName("button");
 const onlyButton2 = document.getElementById("the-only-button");
 const passwordInput = document.getElementById("pass");
 const confirmPass = document.getElementById("confirm"); 
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const telInput = document.querySelector("#phone");
 
 onlyButton2.addEventListener("click", consolation());
 
